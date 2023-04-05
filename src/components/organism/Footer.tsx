14b911152a39428941120b8537d4616c6c676783
Aswini Kumar Rath <aswinirath.sp@gmail.com>
2023-04-05 23:55:42 +0530

    first commit

---
+++ src/components/organism/Footer.tsx
@@ -0,0 +1,23 @@
+import FooterLinks from '@/components/mollecules/FooterLinks'
+
+import { APP_ROUTE } from '@/constant/route'
+//import { socialMedia } from '@/constant/socialMedia'
+import { twclsx } from '@/libs/twclsx'
+
+const Footer = () => {
+  const ownwer = process.env.NEXT_PUBLIC_OWNER_NAME
+
+  return (
+    <footer className={twclsx('layout', 'mt-20 py-10 border-t', 'border-main-2 dark:border-main-3')}>
+      <section className='flex items-stretch gap-8 w-full'>
+        <FooterLinks data={APP_ROUTE} />
+        
+      </section>
+      <section className='mt-10'>
+        
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/blog.tsx
@@ -0,0 +1,62 @@
+import Searchbar from '@/components/mollecules/Searchbar'
+import BlogList from '@/components/organism/BlogList'
+import Hero from '@/components/template/Hero'
+import Layout from '@/components/template/Layout'
+
+import { getBlog } from '@/helpers'
+import { useSearchBlogQuery } from '@/hooks'
+
+import type { GetStaticProps, NextPage } from 'next'
+import { Blog } from 'next-starter-blog'
+
+const meta = {
+  title: 'Blog',
+  description: `This is abou my portfolio and blog`
+}
+
+interface BlogPageProps {
+  latestPost: Array<Blog>
+  allPost: Array<Blog>
+}
+
+const BlogPage: NextPage<BlogPageProps> = ({ latestPost = [], allPost = [] }) => {
+  const { query, handleChange, filteredBlog } = useSearchBlogQuery(allPost)
+
+  return (
+    <Layout as='main' {...meta}>
+      <Hero {...meta} />
+
+      <Searchbar onChange={handleChange} value={query} placeholder='Search Posts..' />
+
+      {query.length === 0 && <BlogList blogs={latestPost} title='Latest Post' className='mb-20' layout='column' />}
+      {query.length === 0 && <BlogList blogs={allPost} title='All Post' layout='column' />}
+
+      {query.length > 0 && filteredBlog.length > 0 ? (
+        <BlogList blogs={filteredBlog} title='Search Result' layout='column' />
+      ) : null}
+
+      {query.length > 0 && filteredBlog.length === 0 ? <p>No result found</p> : null}
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const blogs = await getBlog()
+
+  return {
+    props: {
+      latestPost: blogs
+       
+        .map((b) => ({ ...b.data, slug: b.slug }))
+        
+        .sort((a, b) =>
+          new Date(a.published) > new Date(a.published) ? 1 : new Date(a.published) < new Date(b.published) ? -1 : 0
+        )
+        
+        .slice(0, 2),
+      allPost: blogs.map((b) => ({ ...b.data, slug: b.slug }))
+    }
+  }
+}
+
+export default BlogPage
